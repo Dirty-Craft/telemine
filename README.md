579f9118fafc651dfadb98cb3a87337ab2603b24
parsa shahmaleki <parsampsh@gmail.com>
2023-03-28 18:38:36 +0330

    config: added some help comments

--- README.md
+++ README.md
@@ -14,3 +14,48 @@ These are some features:
 - [ ] Adds `/tg` command that allows players to send a message in the group from the server chat
 - [x] Every feature is configurable and can be enabled/disabled
 - [x] All text messages are customizable in the config
+
+### Configuration
+
+```properties
+# This one can enable/disable the whole mod
+# Allowed values are "true" and "false"
+enabled=false
+
+# Here you have to set bot token and also group chat id
+telegram.bot_token=
+telegram.group_id=
+
+# You can set a proxy for calling the Telegram APIs if you want
+proxy.type=
+proxy.host=
+proxy.port=
+# The proxy.type can be "http" or "socks". Example:
+#proxy.type=socks
+#proxy.host=127.0.0.1
+#proxy.port=1080
+
+# The `feature.*` options can be used to enable/disable different features
+# All the features and enabled by default
+# Allowed values are "true" and "false"
+feature.starting_server_message=true
+feature.server_started_and_ready_message=true
+feature.server_shutdown_message=true
+feature.player_join_messages=true
+feature.player_join_message_show_online_players_list=true
+feature.player_leave_messages=true
+feature.player_leave_message_show_online_players_list=true
+feature.player_death_messages=true
+feature.advancement_made_messages=true
+feature.tg_send_message_command=true
+
+# The `lang.*` options can be used to customize different messages that bot sends in the group
+lang.starting_server_message=Starting the server...
+lang.server_started_and_ready_message=Server is up!
+lang.server_shutdown_message=Shutting down the server...
+lang.online_players_list_message=Online players in the server: {list}
+lang.player_join_message={name} joined the server
+lang.player_left_message={name} left the server
+lang.player_death_message={death_message}
+lang.advancement_made_message={advancement_message}
+```
--- src/main/java/net/dirtycraft/telegrambot/config/ModConfigProvider.java
+++ src/main/java/net/dirtycraft/telegrambot/config/ModConfigProvider.java
@@ -15,13 +15,44 @@ public class ModConfigProvider implements SimpleConfig.DefaultConfig {
 
     public void addKeyValuePair(Pair<String, ?> keyValuePair, String comment) {
         configsList.add(keyValuePair);
+
+        if (keyValuePair.getFirst().equals("enabled")) {
+            configContents += "# This one can enable/disable the whole mod\n" +
+                    "# Allowed values are \"true\" and \"false\"\n";
+        }
+
+        if (keyValuePair.getFirst().equals("telegram.bot_token")) {
+            configContents += "# Here you have to set bot token and also group chat id\n";
+        }
+
+        if (keyValuePair.getFirst().equals("proxy.type")) {
+            configContents += "# You can set a proxy for calling the Telegram APIs if you want\n";
+        }
+
+        if (keyValuePair.getFirst().equals("feature.starting_server_message")) {
+            configContents += "# The `feature.*` options can be used to enable/disable different features\n" +
+                    "# All the features and enabled by default\n" +
+                    "# Allowed values are \"true\" and \"false\"\n";
+        }
+
+        if (keyValuePair.getFirst().equals("lang.starting_server_message")) {
+            configContents += "# The `lang.*` options can be used to customize different messages that bot sends in the group\n";
+        }
+
         configContents += keyValuePair.getFirst() + "=" + keyValuePair.getSecond() + "\n";
+
         if (
                 keyValuePair.getFirst().equals("enabled") ||
                 keyValuePair.getFirst().equals("telegram.group_id") ||
                 keyValuePair.getFirst().equals("feature.tg_send_message_command") ||
                 keyValuePair.getFirst().equals("proxy.port")
         ) {
+            if (keyValuePair.getFirst().equals("proxy.port")) {
+                configContents += "# The proxy.type can be \"http\" or \"socks\". Example:\n" +
+                        "#proxy.type=socks\n" +
+                        "#proxy.host=127.0.0.1\n" +
+                        "#proxy.port=1080\n";
+            }
             configContents += "\n";
         }
     }
