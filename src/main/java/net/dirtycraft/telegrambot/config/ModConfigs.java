849a0c678cde3850f62883d5f701811a1700e4cc
parsa shahmaleki <parsampsh@gmail.com>
2023-03-28 17:47:36 +0330

    added config options for different features

--- src/main/java/net/dirtycraft/telegrambot/config/ModConfigs.java
+++ src/main/java/net/dirtycraft/telegrambot/config/ModConfigs.java
@@ -14,6 +14,16 @@ public class ModConfigs {
     public static String GROUP_ID;
     public static boolean ENABLED;
 
+    public static boolean FEATURE_STARTING_SERVER_MESSAGE;
+    public static boolean FEATURE_SERVER_STARTED_AND_READY_MESSAGE;
+    public static boolean FEATURE_PLAYER_JOIN_MESSAGES;
+    public static boolean FEATURE_PLAYER_JOIN_MESSAGE_SHOW_ONLINE_PLAYERS_LIST;
+    public static boolean FEATURE_PLAYER_LEAVE_MESSAGES;
+    public static boolean FEATURE_PLAYER_LEAVE_MESSAGE_SHOW_ONLINE_PLAYERS_LIST;
+    public static boolean FEATURE_PLAYER_DEATH_MESSAGES;
+    public static boolean FEATURE_ADVANCEMENT_MADE_MESSAGES;
+    public static boolean FEATURE_TG_SEND_MESSAGE_COMMAND;
+
     public static void registerConfigs() {
         configs = new ModConfigProvider();
         createConfigs();
@@ -30,6 +40,16 @@ public class ModConfigs {
         configs.addKeyValuePair(new Pair<>("proxy_host", ""), "String");
         configs.addKeyValuePair(new Pair<>("proxy_port", ""), "String");
         configs.addKeyValuePair(new Pair<>("enabled", false), "boolean");
+
+        configs.addKeyValuePair(new Pair<>("feature.starting_server_message", true), "boolean");
+        configs.addKeyValuePair(new Pair<>("feature.server_started_and_ready_message", true), "boolean");
+        configs.addKeyValuePair(new Pair<>("feature.player_join_messages", true), "boolean");
+        configs.addKeyValuePair(new Pair<>("feature.player_join_message_show_online_players_list", true), "boolean");
+        configs.addKeyValuePair(new Pair<>("feature.player_leave_messages", true), "boolean");
+        configs.addKeyValuePair(new Pair<>("feature.player_leave_message_show_online_players_list", true), "boolean");
+        configs.addKeyValuePair(new Pair<>("feature.player_death_messages", true), "boolean");
+        configs.addKeyValuePair(new Pair<>("feature.advancement_made_messages", true), "boolean");
+        configs.addKeyValuePair(new Pair<>("feature.tg_send_message_command", true), "boolean");
     }
 
     private static void assignConfigs() {
@@ -39,6 +59,16 @@ public class ModConfigs {
         PROXY_TYPE = CONFIG.getOrDefault("proxy_type", "");
         PROXY_HOST = CONFIG.getOrDefault("proxy_host", "");
         PROXY_PORT = CONFIG.getOrDefault("proxy_port", "");
+
+        FEATURE_STARTING_SERVER_MESSAGE = CONFIG.getOrDefault("feature.starting_server_message", true);
+        FEATURE_SERVER_STARTED_AND_READY_MESSAGE = CONFIG.getOrDefault("feature.server_started_and_ready_message", true);
+        FEATURE_PLAYER_JOIN_MESSAGES = CONFIG.getOrDefault("feature.player_join_messages", true);
+        FEATURE_PLAYER_JOIN_MESSAGE_SHOW_ONLINE_PLAYERS_LIST = CONFIG.getOrDefault("feature.player_join_message_show_online_players_list", true);
+        FEATURE_PLAYER_LEAVE_MESSAGES = CONFIG.getOrDefault("feature.player_leave_messages", true);
+        FEATURE_PLAYER_LEAVE_MESSAGE_SHOW_ONLINE_PLAYERS_LIST = CONFIG.getOrDefault("feature.player_leave_message_show_online_players_list", true);
+        FEATURE_PLAYER_DEATH_MESSAGES = CONFIG.getOrDefault("feature.player_death_messages", true);
+        FEATURE_ADVANCEMENT_MADE_MESSAGES = CONFIG.getOrDefault("feature.advancement_made_messages", true);
+        FEATURE_TG_SEND_MESSAGE_COMMAND = CONFIG.getOrDefault("feature.tg_send_message_command", true);
 
         TelegramBot.LOGGER.info("All " + configs.getConfigsList().size() + " config options have been set properly");
     }
