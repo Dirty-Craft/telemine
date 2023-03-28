de07d4658d51e18cd77268ed3f669adb33d3dd6a
parsa shahmaleki <parsampsh@gmail.com>
2023-03-28 14:03:43 +0330

    init

---
+++ src/main/java/net/dirtycraft/telegrambot/TelegramBot.java
@@ -0,0 +1,18 @@
+package net.dirtycraft.telegrambot;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TelegramBot implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("telegrambot");
+
+	@Override
+	public void onInitialize() {
+		//
+	}
+}
